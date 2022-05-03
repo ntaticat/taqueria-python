d1e7ac83661769134a69f9e811821586bfa93073
Rafael Estrada <ntaticat@gmail.com>
2022-05-03 14:35:53 -0500

    update verMenuTacos.py with Rich library

--- verMenuTacos.py
+++ verMenuTacos.py
@@ -3,15 +3,20 @@ from typing import List
 from clasesTacos import *
 from tacosCRUD import *
 
+from rich.console import Console
+from rich.table import Table
+
 def verMenuTacos():
   tacosArr = getTacos()
-  print("---------------------")
-  print("Menu de Tacos")
-  print("---------------------")
-  print("")
+  
+  table = Table(title="Menu de Tacos", title_justify="left")
+  table.add_column("ID", style="magenta", justify="center")
+  table.add_column("Nombre", style="magenta", justify="center")
+  table.add_column("Precio", style="magenta", justify="center")
+  
   for taco in tacosArr:
-    print(f"--> id: {taco.tacoId} - tipo: {taco.nombre} - precio: ${taco.precio}")
-  print("")
+    table.add_row(str(taco.tacoId), taco.nombre, str(taco.precio))
+  console = Console()
+  console.print(table)
+  
   input("Presiona una tecla para continuar...")
-      
-      
